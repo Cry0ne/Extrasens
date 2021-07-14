4a79e4978594645e2e79c8e770d6e8544d38412e
Mr-HAleX <31793029+Mr-HAleX@users.noreply.github.com>
2021-07-15 06:38:02 +0700

    Добавьте файлы проекта.

---
+++ Extrasens/Data/Extrasens.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Extrasens.Data
+{
+    public class Extrasens
+    {
+
+        Random prubRandom = new Random();
+        
+        public int AbraKadabra()
+        {
+            return prubRandom.Next(10, 99);;
+        }
+    }
+}
---
+++ Extrasens/Models/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Extrasens.Models
+{
+    public class History
+    {
+        public int e1;
+        public int e2;
+        public int e3;
+        public int uNumb;
+    }
+}
