afff15174a8dae5603c2e131a930869c71c5ca96
Mr-HAleX <31793029+Mr-HAleX@users.noreply.github.com>
2021-07-22 10:36:16 +0700

    Испрвлено: Логика определения угадавшего; Соответствие стандартам ООП; Реализованы сессии.

---
+++ Extrasens/Data/Psychic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extrasens.Data
+{
+    public class Psychic
+    {
+
+        private readonly Random  rand= new Random();
+
+        private int predictNumber;
+        private int psychicRaiting = 100;       
+        private string responseStatus;
+
+        public int PredictNumber { get => predictNumber; set => predictNumber = value; }
+        public int PsychicRaiting { get => psychicRaiting; set => psychicRaiting = value; }
+        public string ResponseStatus { get => responseStatus; set => responseStatus = value; }
+
+        public void PredictionNumber()
+        {
+            predictNumber = rand.Next(10, 99);
+        }
+
+        public static List<Psychic> Objects = new List<Psychic>();
+
+        public Psychic()
+        {
+            Objects.Add(this);
+        }
+
+
+
+    }
+}
---
+++ Extrasens/Models/PredictionHistory.cs
@@ -0,0 +1,15 @@
+﻿namespace Extrasens.Models
+{
+    public class PredictionHistory
+    {
+        private int predict1;
+        private int predict2;
+        private int predict3;
+        private int uNumb;
+
+        public int Predict1 { get => predict1; set => predict1 = value; }
+        public int Predict2 { get => predict2; set => predict2 = value; }
+        public int Predict3 { get => predict3; set => predict3 = value; }
+        public int UNumb { get => uNumb; set => uNumb = value; }
+    }
+}
